1abcaae5c1eac7ffa91d644c13424c5688ad88e8
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2021-04-21 20:57:08 +0530

    Ajit_Pattern1_Rectangle_Square_Asterisk

---
+++ Awesome_Pattern/Questions_1/Ajit_Pattern1_Rectangle_Square_Asterisk.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+/* main */
+int main() {
+  /* code */
+ int row=1,column=1;
+    cout << "ENTER THE NO. OF [ ROW ] : " << '\n';
+    cin>>row;
+    cout << "ENTER THE NO. OF [ COLUMN ] : " << '\n';
+    cin>>column;
+
+cout <<"------------[ PATTERN ]---------"<<"\n\n";
+
+    for (int i = 0; i < row; i++) {
+      /* code */
+      for (int j=0; j<= column; j++)
+      {
+        cout << "* ";
+      }
+     cout << '\n';
+    }
+
+  return 0;
+}
