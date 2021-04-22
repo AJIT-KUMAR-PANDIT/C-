6a3cceda08258881b2072e649ea8fd4535c4e138
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2021-04-22 13:29:54 +0530

    pattern

---
+++ Awesome_Pattern/Question_3_[_Inverted_Half_Pyramid_]/Ajit_Pattern3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+ int row,column;// initialization of variable
+
+  cout << "ENTER THE NO. OF [ ROW ] : " << '\n';
+  cin>>row;
+
+cout <<"------------[ PATTERN ]---------"<<"\n\n";
+
+  for (int i = row; i > 0; i--) {
+    for (int j = 0; j < i; j++) {
+      cout << "* ";
+    }
+  cout<<"\n";
+  }
+
+return 0;
+}
